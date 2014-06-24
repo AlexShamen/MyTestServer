e94f5cc8b71e5088ef601a74e48d81a7c88beb7f
Alexsandr <alexshamenrilisoft@gmail.com>
2014-06-24 15:49:59 +0400

    добавил: - PhotonServerIn

---
+++ MyTestServerUnityProject/Assets/Scripts/PhotonServerIn.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonServerIn : MonoBehaviour 
+{
+
+	PhotonServer _photonServer;
+
+	void Awake()
+	{
+		if(PhotonServer.Instance != null)
+		{
+			DestroyObject(gameObject);
+		}
+
+//		DontDestroyOnLoad(gameObject);
+//		Application.runInBackground = true;
+	}
+	
+	void Start () 
+	{
+		_photonServer = new PhotonServer();
+		_photonServer.DebugLogHandle = DebugLog;
+	}
+
+	void Update () 
+	{
+		_photonServer.PhotonPeer.Service();
+	}
+
+	void OnApplicationQuit()
+	{
+		_photonServer.PhotonPeer.Disconnect();
+	}
+
+	void DebugLog(object message)
+	{
+		Debug.Log (message);
+	}
+	
+}
