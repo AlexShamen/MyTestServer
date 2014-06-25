de0f2097332ffe7a4180338d6182d5148c4ed34b
Alexsandr <alexshamenrilisoft@gmail.com>
2014-06-25 16:06:08 +0400

    fix

--- MyTestServerUnityProject/Assets/Scripts/PhotonServerIn.cs
+++ MyTestServerUnityProject/Assets/Scripts/PhotonServerIn.cs
@@ -3,29 +3,29 @@ using System.Collections;
 
 public class PhotonServerIn : MonoBehaviour 
 {
-
+	
 	public string Name { get; set; }
 	public GameObject PlayerPrefab;
-
+	
 	PhotonServer _photonServer;
 	float _sendPosInterval = 0.25f;
 	float _currentSendPosInterval = -10.0f;
-
-
-
-
-
+	
+	
+	
+	
+	
 	void Awake()
 	{
 		Name = "bulochka";
-
+		
 		if(PhotonServer.Instance != null)
 		{
 			DestroyObject(gameObject);
 		}
-
-//		DontDestroyOnLoad(gameObject);
-//		Application.runInBackground = true;
+		
+		//		DontDestroyOnLoad(gameObject);
+		//		Application.runInBackground = true;
 	}
 	
 	void Start () 
@@ -34,17 +34,17 @@ public class PhotonServerIn : MonoBehaviour
 		_photonServer.DebugLogHandle = DebugLog;
 		_photonServer.CreatePlayer = CreatePlayer;
 		_photonServer.MovePlayer = MovePlayer;
-
+		
 		Vector3 pos = transform.position;
-		_photonServer.SendOperation_Position(10, pos.x, pos.y, pos.z);
-
+		_photonServer.SendOperation_Position(10, pos);
+		
 		_currentSendPosInterval = _sendPosInterval;
 	}
-
+	
 	void Update () 
 	{
 		_photonServer.PhotonPeer.Service();
-
+		
 		if (_currentSendPosInterval > 0.0f)
 		{
 			_currentSendPosInterval -= Time.deltaTime;
@@ -54,42 +54,42 @@ public class PhotonServerIn : MonoBehaviour
 			if (_currentSendPosInterval > -10.0f)
 			{
 				Vector3 pos = transform.position;
-				_photonServer.SendOperation_Position(20, pos.x, pos.y, pos.z);
+				_photonServer.SendOperation_Position(20, pos);
 				_currentSendPosInterval = _sendPosInterval;
 			}
 		}
 	}
-
+	
 	void OnApplicationQuit()
 	{
 		_photonServer.PhotonPeer.Disconnect();
 	}
-
-
-
-
-
+	
+	
+	
+	
+	
 	void DebugLog(object message)
 	{
 		Debug.Log (message);
 	}
-
-	void CreatePlayer(string id, PhotonServer.MyVector3 position)
+	
+	void CreatePlayer(string id, Vector3 position)
 	{
 		if (id != Name && PlayerPrefab != null)
 		{
 			GameObject obj = (GameObject) GameObject.Instantiate(PlayerPrefab);
 			obj.name = id;
-			obj.transform.position = new Vector3(position.X, position.Y, position.Z);
+			obj.transform.position = position;
 		}
 	}
-
-	void MovePlayer(string id, PhotonServer.MyVector3 position)
+	
+	void MovePlayer(string id, Vector3 position)
 	{
 		if (id!=Name)
 		{
 			GameObject obj = GameObject.Find(id);
-			obj.transform.position = new Vector3(position.X, position.Y, position.Z);
+			obj.transform.position = position;
 		}
 	}
 	
